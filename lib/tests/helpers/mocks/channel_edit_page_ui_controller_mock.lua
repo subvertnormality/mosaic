7849ca2f3dd368eb9f2bead1000c9cfec32478dc
Andrew Hillel <andy@Andrews-MacBook-Pro.local>
2024-04-28 21:09:16 +0100

    Fixing units

--- lib/tests/helpers/mocks/channel_edit_page_ui_controller_mock.lua
+++ lib/tests/helpers/mocks/channel_edit_page_ui_controller_mock.lua
@@ -2,3 +2,6 @@ channel_edit_page_ui_controller = {}
 channel_edit_page_ui_controller.refresh_trig_locks = function() 
   table.insert(channel_edit_page_controller_refresh_trig_locks_events, true)
 end
+channel_edit_page_ui_controller.set_current_note = function(note) 
+  
+end
